a108ee9160cae02c6ddc294fe97ce830f2f19330
Henrik Mellum <henmell99@gmail.com>
2023-10-06 15:03:45 +0200

    Lagt til genom

--- Python/EA/Individual.py
+++ Python/EA/Individual.py
@@ -2,14 +2,20 @@ from deap import base
 
 class _Fitness(base.Fitness):
     def __init__(self):
-        self.weights = (1.0, 1.0)
+        self.weights = 0
 
 class Individual:
-    def __init__(self):
+    def __init__(self, genom ,pos=None):
         self.rng = None
         self.fitness = _Fitness()
         self.distance_moved = None
-        self.map_position = None
+        self.genom = genom
+        self.map_position = pos
 
     def getEndPosition(self):
         return self.map_position
+    
+    def getFitness(self):
+        return self.fitness
+    
+
