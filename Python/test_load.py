56ba0086c869d7af37c26134af1cfc8ebb9aa66e
Henrik Mellum <henmell99@gmail.com>
2023-11-12 20:34:06 +0100

    Update test_load.py

--- Python/test_load.py
+++ Python/test_load.py
@@ -1,6 +1,8 @@
 import pickle
+from qdpy import algorithms, containers, plots
+
     # You may want to import your own packages if the pickle file contains custom objects
 
-with open("iteration-2.p", "rb") as f:
+with open("result/10.11.23/final.p", "rb") as f:
     data = pickle.load(f)
 print(data)
