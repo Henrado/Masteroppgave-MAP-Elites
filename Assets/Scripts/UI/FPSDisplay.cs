33a0f52025e98e61f4c4dea143ee7840cd378aac
Henrik Mellum <henmell99@gmail.com>
2024-01-14 22:26:40 +0100

    Nytt FPS display

--- Assets/Scripts/UI/FPSDisplay.cs
+++ Assets/Scripts/UI/FPSDisplay.cs
@@ -1,26 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+
+// GameObject.FixedUpdate example.
+//
+// Measure frame rate comparing FixedUpdate against Update.
+// Show the rates every second.
 
 public class FPSDisplay : MonoBehaviour
 {
-    public TextMeshProUGUI FpsText;
+    private float updateCount = 0;
+    private float fixedUpdateCount = 0;
+    private float updateUpdateCountPerSecond;
+    private float updateFixedUpdateCountPerSecond;
 
-    private float pollingTime = 1f;
-    private float time;
-    private int frameCount;
-    // Update is called once per frame
+    void Awake()
+    {
+        // Uncommenting this will cause framerate to drop to 10 frames per second.
+        // This will mean that FixedUpdate is called more often than Update.
+        //Application.targetFrameRate = 10;
+        StartCoroutine(Loop());
+    }
+
+    // Increase the number of calls to Update.
     void Update()
     {
-        time += Time.deltaTime;
-        frameCount++;
+        updateCount += 1;
+    }
 
-        if (time >= pollingTime)
+    // Increase the number of calls to FixedUpdate.
+    void FixedUpdate()
+    {
+        fixedUpdateCount += 1;
+    }
+
+    // Show the number of calls to both messages.
+    void OnGUI()
+    {
+        GUIStyle fontSize = new GUIStyle(GUI.skin.GetStyle("label"));
+        fontSize.fontSize = 24;
+        fontSize.normal.textColor = Color.black;
+        GUI.Label(new Rect(10, 10, 200, 50), "Update: " + updateUpdateCountPerSecond.ToString(), fontSize);
+        GUI.Label(new Rect(10, 60, 200, 50), "FixedUpdate: " + updateFixedUpdateCountPerSecond.ToString(), fontSize);
+    }
+
+    // Update both CountsPerSecond values every second.
+    IEnumerator Loop()
+    {
+        while (true)
         {
-            int frameRate = Mathf.RoundToInt(frameCount/time);
-            FpsText.text = frameRate.ToString() + " FPS";
+            yield return new WaitForSeconds(1);
+            updateUpdateCountPerSecond = updateCount;
+            updateFixedUpdateCountPerSecond = fixedUpdateCount;
 
-            time -= pollingTime;
-            frameCount = 0;
+            updateCount = 0;
+            fixedUpdateCount = 0;
         }
     }
 }
