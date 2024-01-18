56ac880bd9c72bf42069da0dcc551ca42a0fd70d
Henrik Mellum <henmell99@gmail.com>
2024-01-18 15:58:04 +0100

    Nye kontrollere og individ
    To nye kontrollere
    Et individ som låser alle variablene til alle serover

--- Python/EA/Controllers.py
+++ Python/EA/Controllers.py
@@ -41,6 +41,27 @@ class SineController(Controller):
     def get_count_possibleLoc(cls) -> int:
         return 2
     
+class SineControllerUfq(Controller):
+    def __init__(self, A : float, phi : float, theta : float):
+        # Setup initial controller values
+        self.amplitude = A
+        self.amplitude_scale = 1.0
+
+        self.phi = phi*np.pi
+        self.theta = theta
+
+    def get_action(self, time, observation=None) -> float:
+        controller_value = self.amplitude * np.sin(2*np.pi*time + self.phi) + self.theta
+        return np.clip(controller_value, -self.amplitude_scale, self.amplitude_scale)
+    
+    @classmethod
+    def get_count_variables(cls) -> int:
+        return 3
+    
+    @classmethod
+    def get_count_possibleLoc(cls) -> int:
+        return 1
+    
 class TanhController(Controller):
     def __init__(self, A : float, theta : float):
         # Setup initial controller values
@@ -62,16 +83,16 @@ class TanhController(Controller):
         return 1
 
 class TanhControllerWOff(Controller):
-    def __init__(self, A : float, theta : float, offset : float):
+    def __init__(self, A : float, phi : float, theta : float):
         # Setup initial controller values
         self.amplitude = A
-        self.offset = offset
+        self.theta = theta
         self.amplitude_scale = 1.0
 
-        self.theta = theta
+        self.phi = phi
 
     def get_action(self, time, observation=None) -> float:
-        controller_value = self.amplitude * np.tanh(4*np.sin(2*np.pi*(time + self.theta))) + self.offset
+        controller_value = self.amplitude * np.tanh(4*np.sin(2*np.pi*(time + self.phi))) + self.theta
         return np.clip(controller_value, -self.amplitude_scale, self.amplitude_scale)
     
     @classmethod
@@ -81,3 +102,25 @@ class TanhControllerWOff(Controller):
     @classmethod
     def get_count_possibleLoc(cls) -> int:
         return 1
+
+class TanhControllerWOffFq(Controller):
+    def __init__(self, A : float, f : float, phi : float, theta : float):
+        # Setup initial controller values
+        self.amplitude = A
+        self.theta = theta
+        self.f = f
+        self.amplitude_scale = 1.0
+
+        self.phi = phi
+
+    def get_action(self, time, observation=None) -> float:
+        controller_value = self.amplitude * np.tanh(4*np.sin(2*np.pi*(time*self.f + self.phi))) + self.theta
+        return np.clip(controller_value, -self.amplitude_scale, self.amplitude_scale)
+    
+    @classmethod
+    def get_count_variables(cls) -> int:
+        return 4
+    
+    @classmethod
+    def get_count_possibleLoc(cls) -> int:
+        return 2
--- Python/EA/Individual.py
+++ Python/EA/Individual.py
@@ -82,6 +82,12 @@ class Individual_twoLock:
             A_all = np.concatenate([A for _ in range(self.COUNT_LEG)])[:, np.newaxis]
             F_all = np.concatenate([F for _ in range(self.COUNT_LEG)])[:, np.newaxis]
             genom = np.concatenate([A_all, F_all, rest], axis=1).reshape((self.COUNT_LEG, self.ACTUATOR_LEG, self.PARAMS_ACTUATORS))
+        elif issubclass(controller, SineControllerUfq):
+            A = np.array(genom_raw[0:3])
+            rest = np.array(genom_raw[3:]).reshape((self.COUNT_LEG*self.ACTUATOR_LEG, 2))
+
+            A_all = np.concatenate([A for _ in range(self.COUNT_LEG)])[:, np.newaxis]
+            genom = np.concatenate([A_all, rest], axis=1).reshape((self.COUNT_LEG, self.ACTUATOR_LEG, self.PARAMS_ACTUATORS))
         elif issubclass(controller, TanhController):
             A = np.array(genom_raw[0:3])
             rest = np.array(genom_raw[3:]).reshape((self.COUNT_LEG*self.ACTUATOR_LEG, controller.get_count_possibleLoc()))
@@ -94,6 +100,14 @@ class Individual_twoLock:
 
             A_all = np.concatenate([A for _ in range(self.COUNT_LEG)])[:, np.newaxis]
             genom = np.concatenate([A_all, rest], axis=1).reshape((self.COUNT_LEG, self.ACTUATOR_LEG, self.PARAMS_ACTUATORS))
+        elif issubclass(controller, TanhControllerWOffFq):
+            A = np.array(genom_raw[0:3])
+            F = np.array(genom_raw[3:6])
+            rest = np.array(genom_raw[6:]).reshape((self.COUNT_LEG*self.ACTUATOR_LEG, 2))
+
+            A_all = np.concatenate([A for _ in range(self.COUNT_LEG)])[:, np.newaxis]
+            F_all = np.concatenate([F for _ in range(self.COUNT_LEG)])[:, np.newaxis]
+            genom = np.concatenate([A_all, F_all, rest], axis=1).reshape((self.COUNT_LEG, self.ACTUATOR_LEG, self.PARAMS_ACTUATORS))
         else:
             genom = np.zeros((self.COUNT_LEG, self.ACTUATOR_LEG, self.PARAMS_ACTUATORS))
 
@@ -120,11 +134,96 @@ class Individual_twoLock:
     def get_dimension_shape(cls, con:Controller):
         cls.PARAMS_ACTUATORS = con.get_count_variables()
         return (cls.COUNT_LEG, cls.ACTUATOR_LEG, cls.PARAMS_ACTUATORS)
+    
+
+class Individual_globalLock:
+    COUNT_A = 3
+    COUNT_F = 3
+
+    COUNT_LEG = 4
+    ACTUATOR_LEG = 3
+    PARAMS_ACTUATORS = 2
+    COUNT_FI_TH = COUNT_LEG * ACTUATOR_LEG * PARAMS_ACTUATORS
+
+    def __init__(self, genom, controller):
+        self.genom = genom
+        self.controllers = self._initControllers(controller, genom)
+
+    def _initControllers(self, controller, genom_raw):
+        # genom_raw er lagt opp slik:
+        # [A_body, A_upperlegg, A_loverleg, f_body, f_upperleg, 
+        # f_loverleg, phi_con1, theta_con1, phi_con2, theta_con2...]
+
+        # Skal bli slik:
+        # [[A_body, f_body, phi_con1, theta_con1]
+        #  [A_upperleg, f_upperleg, phi_con2, theta_con2]
+        #  ...]
+        if issubclass(controller, SineController):
+            A = np.array([genom_raw[0] for _ in range(self.ACTUATOR_LEG)])
+            F = np.array([genom_raw[1] for _ in range(self.ACTUATOR_LEG)])
+            rest = np.array(genom_raw[2:]).reshape((self.COUNT_LEG*self.ACTUATOR_LEG, controller.get_count_possibleLoc()))
+
+            A_all = np.concatenate([A for _ in range(self.COUNT_LEG)])[:, np.newaxis]
+            F_all = np.concatenate([F for _ in range(self.COUNT_LEG)])[:, np.newaxis]
+            genom = np.concatenate([A_all, F_all, rest], axis=1).reshape((self.COUNT_LEG, self.ACTUATOR_LEG, 4))
+        elif issubclass(controller, SineControllerUfq):
+            A = np.array([genom_raw[0] for _ in range(self.ACTUATOR_LEG)])
+            rest = np.array(genom_raw[1:]).reshape((self.COUNT_LEG*self.ACTUATOR_LEG, 2))
+
+            A_all = np.concatenate([A for _ in range(self.COUNT_LEG)])[:, np.newaxis]
+            genom = np.concatenate([A_all, rest], axis=1).reshape((self.COUNT_LEG, self.ACTUATOR_LEG, self.PARAMS_ACTUATORS))
+        elif issubclass(controller, TanhController):
+            A = np.array([genom_raw[0] for _ in range(self.ACTUATOR_LEG)])
+            rest = np.array(genom_raw[1:]).reshape((self.COUNT_LEG*self.ACTUATOR_LEG, controller.get_count_possibleLoc()))
+
+            A_all = np.concatenate([A for _ in range(self.COUNT_LEG)])[:, np.newaxis]
+            genom = np.concatenate([A_all, rest], axis=1).reshape((self.COUNT_LEG, self.ACTUATOR_LEG, self.PARAMS_ACTUATORS))
+        elif issubclass(controller, TanhControllerWOff):
+            A = np.array([genom_raw[0] for _ in range(self.ACTUATOR_LEG)])
+            rest = np.array(genom_raw[1:]).reshape((self.COUNT_LEG*self.ACTUATOR_LEG, 2))
+
+            A_all = np.concatenate([A for _ in range(self.COUNT_LEG)])[:, np.newaxis]
+            genom = np.concatenate([A_all, rest], axis=1).reshape((self.COUNT_LEG, self.ACTUATOR_LEG, self.PARAMS_ACTUATORS))
+        elif issubclass(controller, TanhControllerWOffFq):
+            A = np.array([genom_raw[0] for _ in range(self.ACTUATOR_LEG)])
+            F = np.array([genom_raw[1] for _ in range(self.ACTUATOR_LEG)])
+            rest = np.array(genom_raw[2:]).reshape((self.COUNT_LEG*self.ACTUATOR_LEG, 2))
+
+            A_all = np.concatenate([A for _ in range(self.COUNT_LEG)])[:, np.newaxis]
+            F_all = np.concatenate([F for _ in range(self.COUNT_LEG)])[:, np.newaxis]
+            genom = np.concatenate([A_all, F_all, rest], axis=1).reshape((self.COUNT_LEG, self.ACTUATOR_LEG, self.PARAMS_ACTUATORS))
+        else:
+            genom = np.zeros((self.COUNT_LEG, self.ACTUATOR_LEG, self.PARAMS_ACTUATORS))
+
+        controllers = []
+        for leg in range(len(genom)):
+            for actuator in range(len(genom[leg])):
+                controllers.append(controller(*genom[leg, actuator].T)) # type: ignore
+        return controllers
+
+    def get_actions(self, time : float):
+        action_angels = np.zeros((1,len(self.controllers)))
+        for i in range(len(self.controllers)):
+            action_angels[0,i] = self.controllers[i].get_action(time)
+        return action_angels
+    
+    @classmethod
+    def get_dimension_count(cls, con:Controller):
+        cls.PARAMS_ACTUATORS = con.get_count_variables()
+        return con.get_count_possibleLoc()+(cls.COUNT_LEG*(con.get_count_variables()-con.get_count_possibleLoc()))*cls.ACTUATOR_LEG
+        #return cls.COUNT_A+cls.COUNT_F+cls.COUNT_LEG*cls.ACTUATOR_LEG*cls.PARAMS_ACTUATORS
+    
+    @classmethod
+    def get_dimension_shape(cls, con:Controller):
+        cls.PARAMS_ACTUATORS = con.get_count_variables()
+        return (cls.COUNT_LEG, cls.ACTUATOR_LEG, cls.PARAMS_ACTUATORS)
 
 
 if __name__ == "__main__":
-    s = SineController
-    ind = Individual_zeroLocked
+    s = TanhControllerWOffFq
+    #ind = Individual_zeroLocked
+    ind = Individual_twoLock
+    ind = Individual_globalLock
     count = ind.get_dimension_count(s) # type: ignore
     genom = np.arange(count)
     print(genom)
