213388bbbade9f6c55a3dc9dede8c5f07d0b4459
Henrik Mellum <henmell99@gmail.com>
2024-01-30 12:32:44 +0100

    Bruker nå heller EOT og variabelbytte

--- Python/slurm.sh
+++ Python/slurm.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/bash
-#SBATCH --array=1-100
-#SBATCH --ntasks=1
+sbatch<<EOT
+#!/usr/bin/bash
+#SBATCH -J $1 #job_name
+#SBATCH --array=1-$2
 #SBATCH --mem=2G
+#SBATCH --ntasks=1
+#SBATCH --output=result/$1/%a/output.txt
 #SBATCH --cpus-per-task=2
+#$4
 
 source ~/.bashrc
 conda activate env39
 
-srun python main3.py -c conf/$1.yaml -w ${SLURM_ARRAY_TASK_ID} -o $1/${SLURM_ARRAY_TASK_ID}
-
+srun python main3.py -c conf/$1.yaml -w $3 -o $1/$3
 sleep 60
+
+EOT
