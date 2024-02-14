73f1a2811d04556ff29809743cb6f613a7c270d0
Henrik Mellum <henmell99@gmail.com>
2024-02-14 15:41:04 +0100

    Alle eksperimentene

--- Python/master_slurm.sh
+++ Python/master_slurm.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 # Array med alle configfilene 
-array=( Z_TWoff_B_exLimit Z_TWoffFq_B_exLimit G_S_B_exLimit G_SUfq_B_exLimit G_T_B_exLimit G_TWoff_B_exLimit G_TWoffFq_B_exLimit T_S_B_exLimit T_SUfq_B_exLimit T_T_B_exLimit T_TWoff_B_exLimit T_TWoffFq_B_exLimit )
+array=( Z_S_B_exLimit Z_SUfq_B_exLimit Z_T_B_exLimit Z_TWoff_B_exLimit Z_TWoffFq_B_exLimit G_S_B_exLimit G_SUfq_B_exLimit G_T_B_exLimit G_TWoff_B_exLimit G_TWoffFq_B_exLimit T_S_B_exLimit T_SUfq_B_exLimit T_T_B_exLimit T_TWoff_B_exLimit T_TWoffFq_B_exLimit )
 count_ex=100
 eksperiment_navn="Determ"
 echo ${array[0]}
