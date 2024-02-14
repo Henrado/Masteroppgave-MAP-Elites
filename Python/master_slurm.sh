b56e926255ca75b1bff4881afa4733c1cb53db4b
Henrik Mellum <henmell99@gmail.com>
2024-02-14 15:37:12 +0100

    Mulighet for å ha et felles eksperimentnavn

--- Python/master_slurm.sh
+++ Python/master_slurm.sh
@@ -2,14 +2,15 @@
 # Array med alle configfilene 
 array=( Z_TWoff_B_exLimit Z_TWoffFq_B_exLimit G_S_B_exLimit G_SUfq_B_exLimit G_T_B_exLimit G_TWoff_B_exLimit G_TWoffFq_B_exLimit T_S_B_exLimit T_SUfq_B_exLimit T_T_B_exLimit T_TWoff_B_exLimit T_TWoffFq_B_exLimit )
 count_ex=100
+eksperiment_navn="Determ"
 echo ${array[0]}
 for i in "${array[@]}";
 do
         if [[ $i == $array ]]
         then
-                jobstr=$(bash slurm.sh $i $count_ex '${SLURM_ARRAY_TASK_ID}')
+                jobstr=$(bash slurm.sh $i $count_ex '${SLURM_ARRAY_TASK_ID}' $eksperiment_navn)
         else
-                jobstr=$(bash slurm.sh $i $count_ex '${SLURM_ARRAY_TASK_ID}' "SBATCH --dependency=aftercorr:${jobstr##* }")
+                jobstr=$(bash slurm.sh $i $count_ex '${SLURM_ARRAY_TASK_ID}' $eksperiment_navn "SBATCH --dependency=aftercorr:${jobstr##* }")
         fi
         echo $jobstr
 done
--- Python/slurm.sh
+++ Python/slurm.sh
@@ -5,14 +5,14 @@ sbatch<<EOT
 #SBATCH --array=1-$2
 #SBATCH --mem=2G
 #SBATCH --ntasks=1
-#SBATCH --output=result/$1/%a/output.txt
+#SBATCH --output=result/$4/$1/%a/output.txt
 #SBATCH --cpus-per-task=2
-#$4
+#$5
 
 source ~/.bashrc
 conda activate env39
 
-srun python main3.py -c conf/$1.yaml -w $3 -o $1/$3
+srun python main3.py -c conf/$1.yaml -w $3 -o $4/$1/$3
 sleep 60
 
 EOT
