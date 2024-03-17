a60270f8b2357140f8c0759f2f6a04e71c9e04ea
Henrik Mellum <henmell99@gmail.com>
2024-03-17 19:59:41 +0100

    Lagde start/stop til slurm

--- Python/master_slurm.sh
+++ Python/master_slurm.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/bash
 # Array med alle configfilene 
 array=( Z_S_B_exLimit T_S_B_exLimit G_S_B_exLimit Z_SUfq_B_exLimit Z_T_B_exLimit Z_TWoff_B_exLimit Z_TWoffFq_B_exLimit G_SUfq_B_exLimit G_T_B_exLimit G_TWoff_B_exLimit G_TWoffFq_B_exLimit T_SUfq_B_exLimit T_T_B_exLimit T_TWoff_B_exLimit T_TWoffFq_B_exLimit )
-count_ex=100
+start=1
+stop=100
 eksperiment_navn="Determ"
 echo ${array[0]}
 for i in "${array[@]}";
 do
         if [[ $i == $array ]]
         then
-                jobstr=$(bash slurm.sh $i $count_ex '${SLURM_ARRAY_TASK_ID}' $eksperiment_navn)
+                jobstr=$(bash slurm.sh $i $start $stop '${SLURM_ARRAY_TASK_ID}' $eksperiment_navn)
         else
-                jobstr=$(bash slurm.sh $i $count_ex '${SLURM_ARRAY_TASK_ID}' $eksperiment_navn "SBATCH --dependency=aftercorr:${jobstr##* }")
+                jobstr=$(bash slurm.sh $i $start $stop '${SLURM_ARRAY_TASK_ID}' $eksperiment_navn "SBATCH --dependency=aftercorr:${jobstr##* }")
         fi
         echo $jobstr
 done
--- Python/slurm.sh
+++ Python/slurm.sh
@@ -2,17 +2,17 @@
 sbatch<<EOT
 #!/usr/bin/bash
 #SBATCH -J $1 #job_name
-#SBATCH --array=1-$2
+#SBATCH --array=$2-$3
 #SBATCH --mem=3G
 #SBATCH --ntasks=1
-#SBATCH --output=result/$4/$1/%a/output.txt
+#SBATCH --output=result/$5/$1/%a/output.txt
 #SBATCH --cpus-per-task=2
-#$5
+#$6
 
 source ~/.bashrc
 conda activate env39
 
-srun python main3.py -c conf/$1.yaml -w $3 -o $4/$1/$3
+srun python main3.py -c conf/$1.yaml -w $4 -o $5/$1/$4
 sleep 60
 
 EOT
