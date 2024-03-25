8b0fe92c181d19111224e5a881d681cb7dcaa382
Henrik Mellum <henmell99@gmail.com>
2024-03-25 13:28:19 +0100

    Automatisk lage plot

--- Python/akumulertplott.py
+++ Python/akumulertplott.py
@@ -53,6 +53,7 @@ def dataframe2numpy(dataframes:np.ndarray, key:str="", dtype=None):
 def plot_std_line(experiments:list, scale:float=1, output_filename=None, title="", xlabel="Evaluations", ylabel=""):
     t = np.arange(experiments[0]["data"][0].shape[0])*scale
     fig, ax = plt.subplots(1)
+    all_means = []
 
     for dic in experiments:
         arr = dic["data"]
@@ -64,11 +65,15 @@ def plot_std_line(experiments:list, scale:float=1, output_filename=None, title="
         # plot it!
         ax.plot(t, mu1, lw=2, label=dic["label"], color=dic["color"])
         ax.fill_between(t, mu1+sigma1, mu1-sigma1, facecolor=dic["color"], alpha=0.5)
+        print(dic["label"], round(mu1[-1], 1))
+        all_means.append(mu1[-1])
+    print("All means:", round(np.mean(all_means), 1), "\n")
     ax.set_title(title)
     ax.legend(loc='upper left')
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
     ax.grid()
+    plt.tight_layout()
     if output_filename is not None:
         fig.savefig(output_filename)
         plt.close(fig)
@@ -95,39 +100,60 @@ def do_it_all_varShow():
 #container_shape = config["containers"][config["algorithms"]["container"]]["shape"]
 
 determ = [
-    {"path": "../../Determ/T_S_B_exLimit", "label": "T_S_B_exLimit", "color": "black"},
-    {"path": "../../Determ/Z_S_B_exLimit", "label": "Z_S_B_exLimit", "color": "grey"},
-    {"path": "../../Determ/G_S_B_exLimit", "label": "G_S_B_exLimit", "color": "magenta"},
+    {"path": "../../Master_Resultater/Determ/T_S_B_exLimit", "label": "T_S_B_exLimit", "color": "black"},
+    {"path": "../../Master_Resultater/Determ/Z_S_B_exLimit", "label": "Z_S_B_exLimit", "color": "grey"},
+    {"path": "../../Master_Resultater/Determ/G_S_B_exLimit", "label": "G_S_B_exLimit", "color": "magenta"},
 
-    {"path": "../../Determ/T_SUfq_B_exLimit", "label": "T_SUfq_B_exLimit", "color": "red"},
-    {"path": "../../Determ/Z_SUfq_B_exLimit", "label": "Z_SUfq_B_exLimit", "color": "lightcoral"},
-    {"path": "../../Determ/G_SUfq_B_exLimit", "label": "G_SUfq_B_exLimit", "color": "peru"},
+    {"path": "../../Master_Resultater/Determ/T_SUfq_B_exLimit", "label": "T_SUfq_B_exLimit", "color": "red"},
+    {"path": "../../Master_Resultater/Determ/Z_SUfq_B_exLimit", "label": "Z_SUfq_B_exLimit", "color": "lightcoral"},
+    {"path": "../../Master_Resultater/Determ/G_SUfq_B_exLimit", "label": "G_SUfq_B_exLimit", "color": "peru"},
 
-    {"path": "../../Determ/T_T_B_exLimit", "label": "T_T_B_exLimit", "color": "royalblue"},
-    {"path": "../../Determ/Z_T_B_exLimit", "label": "Z_T_B_exLimit", "color": "navy"},
-    {"path": "../../Determ/G_T_B_exLimit", "label": "G_T_B_exLimit", "color": "indigo"},
+    {"path": "../../Master_Resultater/Determ/T_T_B_exLimit", "label": "T_T_B_exLimit", "color": "royalblue"},
+    {"path": "../../Master_Resultater/Determ/Z_T_B_exLimit", "label": "Z_T_B_exLimit", "color": "navy"},
+    {"path": "../../Master_Resultater/Determ/G_T_B_exLimit", "label": "G_T_B_exLimit", "color": "indigo"},
 
-    {"path": "../../Determ/T_TWoff_B_exLimit", "label": "T_TWoff_B_exLimit", "color": "gold"},
-    {"path": "../../Determ/Z_TWoff_B_exLimit", "label": "Z_TWoff_B_exLimit", "color": "orange"},
-    {"path": "../../Determ/G_TWoff_B_exLimit", "label": "G_TWoff_B_exLimit", "color": "tan"},
+    {"path": "../../Master_Resultater/Determ/T_TWoff_B_exLimit", "label": "T_TWoff_B_exLimit", "color": "gold"},
+    {"path": "../../Master_Resultater/Determ/Z_TWoff_B_exLimit", "label": "Z_TWoff_B_exLimit", "color": "orange"},
+    {"path": "../../Master_Resultater/Determ/G_TWoff_B_exLimit", "label": "G_TWoff_B_exLimit", "color": "tan"},
 
-    {"path": "../../Determ/T_TWoffFq_B_exLimit", "label": "T_TWoffFq_B_exLimit", "color": "forestgreen"},
-    {"path": "../../Determ/Z_TWoffFq_B_exLimit", "label": "Z_TWoffFq_B_exLimit", "color": "lime"},
-    {"path": "../../Determ/G_TWoffFq_B_exLimit", "label": "G_TWoffFq_B_exLimit", "color": "palegreen"}
+    {"path": "../../Master_Resultater/Determ/T_TWoffFq_B_exLimit", "label": "T_TWoffFq_B_exLimit", "color": "forestgreen"},
+    {"path": "../../Master_Resultater/Determ/Z_TWoffFq_B_exLimit", "label": "Z_TWoffFq_B_exLimit", "color": "lime"},
+    {"path": "../../Master_Resultater/Determ/G_TWoffFq_B_exLimit", "label": "G_TWoffFq_B_exLimit", "color": "palegreen"}
 ]
 
 miljo = [
-    {"path": "../../Miljo/Z_TWoff_B_CS0", "label": "CubeSize=0", "color": "lime"},
-    {"path": "../../Miljo/Z_TWoff_B_CS1", "label": "CubeSize=1", "color": "magenta"},
-    {"path": "../../Miljo/Z_TWoff_B_CS2", "label": "CubeSize=2", "color": "red"},
-    {"path": "../../Miljo/Z_TWoff_B_CS5", "label": "CubeSize=5", "color": "navy"},
-    {"path": "../../Miljo/Z_TWoff_B_HCS1", "label": "CubeSize=1 Half", "color": "gold"},
-    {"path": "../../Miljo/Z_TWoff_B_HCS2", "label": "CubeSize=2 Half", "color": "forestgreen"},
-    {"path": "../../Miljo/Z_TWoff_B_HCS5", "label": "CubeSize=5 Half", "color": "black"}
+    {"path": "../../Master_Resultater/Miljo/Z_TWoff_B_CS0", "label": "CubeSize=0", "color": "lime"},
+    {"path": "../../Master_Resultater/Miljo/Z_TWoff_B_CS1", "label": "CubeSize=1", "color": "magenta"},
+    {"path": "../../Master_Resultater/Miljo/Z_TWoff_B_CS2", "label": "CubeSize=2", "color": "red"},
+    {"path": "../../Master_Resultater/Miljo/Z_TWoff_B_CS5", "label": "CubeSize=5", "color": "navy"},
+    {"path": "../../Master_Resultater/Miljo/Z_TWoff_B_HCS1", "label": "CubeSize=1 Half", "color": "gold"},
+    {"path": "../../Master_Resultater/Miljo/Z_TWoff_B_HCS2", "label": "CubeSize=2 Half", "color": "forestgreen"},
+    {"path": "../../Master_Resultater/Miljo/Z_TWoff_B_HCS5", "label": "CubeSize=5 Half", "color": "black"}
 ]
-ex_lost_dict = determ
-do_it_all_stdline(ex_lost_dict, "iterations.csv", "qd_score", title="QD_score", scale=True)
-do_it_all_stdline(ex_lost_dict, "evals.csv", "cont_size", title="Konteiner fylt")
+two  = [determ[0], determ[3], determ[6], determ[9] , determ[12]]
+zero = [determ[1], determ[4], determ[7], determ[10], determ[13]]
+glob = [determ[2], determ[5], determ[8], determ[11], determ[14]]
+
+s       = [determ[0], determ[1], determ[2]]
+sufq    = [determ[3], determ[4], determ[5]]
+t       = [determ[6], determ[7], determ[8]]
+tWoff   = [determ[9], determ[10], determ[11]]
+tWoffFq = [determ[12], determ[13], determ[14]]
+
+ex_lost_dict = miljo
+#do_it_all_stdline(ex_lost_dict, "iterations.csv", "qd_score", title="QD_score", scale=True)
+#do_it_all_stdline(ex_lost_dict, "evals.csv", "cont_size", title="Konteiner fylt")
+
+""" do_it_all_stdline(zero, "iterations.csv", "qd_score", title="QD_score for Zero", scale=True, output_filename="QD_score_zero.svg")
+do_it_all_stdline(two, "iterations.csv", "qd_score", title="QD_score for Two", scale=True, output_filename="QD_score_two.svg")
+do_it_all_stdline(glob, "iterations.csv", "qd_score", title="QD_score for Global", scale=True, output_filename="QD_score_global.svg")
+
+do_it_all_stdline(s, "iterations.csv", "qd_score", title="QD_score for Sinus", scale=True, output_filename="QD_score_S.svg")
+do_it_all_stdline(sufq, "iterations.csv", "qd_score", title="QD_score for Sinus uten frekvens", scale=True, output_filename="QD_score_SUfq.svg")
+do_it_all_stdline(t, "iterations.csv", "qd_score", title="QD_score for Tanh", scale=True, output_filename="QD_score_T.svg")
+do_it_all_stdline(tWoff, "iterations.csv", "qd_score", title="QD_score for Tanh med offset", scale=True, output_filename="QD_score_TWoff.svg")
+do_it_all_stdline(tWoffFq, "iterations.csv", "qd_score", title="QD_score for Tanh med offset og frekvens", scale=True, output_filename="QD_score_TWoffFq.svg") """
+
 def do_it_all_grid(path:str, filename:str, output_filename:str, quality_array:bool, type_operation:str, scale:float=1):
     d, config = get_all_dataframes(path, filename=filename)
     arr2 = dataframe2numpy(d)
@@ -160,15 +186,17 @@ def do_it_all_grid(path:str, filename:str, output_filename:str, quality_array:bo
         plots.plotGridSubplots(data, os.path.join(path, output_filename), plt.get_cmap("Reds", max_activity), features_domain, [0, max_activity], nbTicks=None) # type: ignore
 
 
-path = "../../resultater2/G_S_B_exLimit"
-path = "../../resultater2/Z_S_B_exLimit"
-filname = "grid.quality_array.csv"
-output_filename = "performancesGrid.svg"
-#do_it_all_grid(path, filname, output_filename, True, "mean")
 
-filname = "grid.activity_per_bin.csv"
-output_filename = "grid.activity_per_bin.svg"
-#do_it_all_grid(path, filname, output_filename, False, "max")
+for i in ex_lost_dict:
+    filname = "grid.quality_array.csv"
+    path = i["path"]
+    output_filename = i["label"] + "_performancesGrid_mean.svg"
+    do_it_all_grid(path, filname, output_filename, True, "mean")
+
+    filname = "grid.activity_per_bin.csv"
+    output_filename = i["label"] + "_grid.activity_per_bin_mean.svg"
+    do_it_all_grid(path, filname, output_filename, False, "mean")
+    plt.close()
 
 """ d, conf = get_all_dataframes(path, "grid.solutions.csv", parse=True)
 arr2 = dataframe2numpy(d, dtype=object)
