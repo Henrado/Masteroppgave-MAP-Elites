2a514e0f0c1d5fb2e963869df17f74b73e41e6f9
Henrik Mellum <henmell99@gmail.com>
2024-04-23 15:45:01 +0200

    For timescale=1

--- Python/track.py
+++ Python/track.py
@@ -38,26 +38,27 @@ def prepare_sim(csv):
 def do_it_all(ax, base_dir_real, base_dir_sim, ex_name):
     real_dir = base_dir_real + ex_name + "/"
     csv_files = [f for f in os.listdir(real_dir) if f.endswith('.csv')]
-    sim_filename_uten = base_dir_sim + "Ubuntu20_utenkuber/" + ex_name + "_exLimit_utenkuber.csv"
-    sim_filename_med = base_dir_sim + "Ubuntu20_medkuber/" + ex_name + "_exLimit_medkuber.csv"
+    sim_filename_uten = base_dir_sim + "" + ex_name + "_exLimit_utenkuber.csv"
+    sim_filename_med = base_dir_sim + "" + ex_name + "_exLimit_medkuber.csv"
 
     sim_rename_dict = {"x": "X_pos_sim", "z": "Z_pos_sim", "y_rot": "Y_rot_sim"}
     real_rename_dict = {"X":"X_rot", "Y":"Y_rot", "Z":"Z_rot", "X.1":"X_pos", "Y.1": "Y_pos", "Z.1": "Z_pos"}
     
 
     #fig, ax = plt.subplots(figsize=(5.5, 4))
-    sim_uten_csv = read_csv(sim_filename_uten, index_col=0, rename=sim_rename_dict)
-    sim_uten_csv = prepare_sim(sim_uten_csv)
-    print(ax)
-    ax.plot(sim_uten_csv.loc[:,"Z_pos_sim"], sim_uten_csv.loc[:,"X_pos_sim"], label="Simulering uten kuber")
-
     sim_med_csv = read_csv(sim_filename_med, index_col=0, rename=sim_rename_dict)
     sim_med_csv = prepare_sim(sim_med_csv)
+
+    sim_uten_csv = read_csv(sim_filename_uten, index_col=0, rename=sim_rename_dict)
+    sim_uten_csv = prepare_sim(sim_uten_csv)
+    
+    ax.plot(sim_med_csv.loc[:,"Z_pos_sim"], sim_med_csv.loc[:,"X_pos_sim"], label="Simulering med kuber")
+    ax.plot(sim_uten_csv.loc[:,"Z_pos_sim"], sim_uten_csv.loc[:,"X_pos_sim"], label="Simulering uten kuber")
     
     sum_lengde_sim_med = np.sqrt(sim_med_csv.iloc[-1]["X_pos_sim"]**2+sim_med_csv.iloc[-1]["Z_pos_sim"]**2)
     sum_lengde_sim_uten = np.sqrt(sim_uten_csv.iloc[-1]["X_pos_sim"]**2+sim_uten_csv.iloc[-1]["Z_pos_sim"]**2)
 
-    ax.plot(sim_med_csv.loc[:,"Z_pos_sim"], sim_med_csv.loc[:,"X_pos_sim"], label="Simulering med kuber")
+    
     all_csvs = []
     sum_lengde_real = 0
     for ind, i in enumerate(csv_files):
@@ -98,7 +99,18 @@ if __name__ == "__main__":
     #ex_names = ["X-retning", "Z-retning"]
     #base_dir_real = "../../Master_Resultater/FysiskTest/Baseline/Mocap/"
     #base_dir_sim  = "../../Master_Resultater/FysiskTest/Baseline/Sim/"
-    fig, axs = plt.subplots(4, 2,figsize=(5, 9),  sharex=True, sharey=True)
+
+    ex_names = ["CS0", "CS1","CS5","HCS2","HCS5"]
+    base_dir_real = "../../Master_Resultater/FysiskTestTimescale/Miljo/Mocap/"
+    base_dir_sim  = "../../Master_Resultater/FysiskTestTimescale/Miljo/Sim/"
+
+    ex_names = ["G_S_B", "G_SUfq_B", "G_T_B", "G_TWoff_B", "G_TWoffFq_B"]
+    ex_names = ["T_SUfq_B", "T_T_B", "T_TWoff_B"]
+    ex_names = ["Z_S_B", "Z_SUfq_B", "Z_T_B", "Z_TWoff_B", "Z_TWoffFq_B"]
+    base_dir_real = "../../Master_Resultater/FysiskTestTimescale/Determ/Mocap/"
+    base_dir_sim  = "../../Master_Resultater/FysiskTestTimescale/Determ/Sim/"
+
+    fig, axs = plt.subplots(2, 3,figsize=(9, 5),  sharex=True, sharey=True)
     #axs = axs.reshape(-1)
     for i, ax in zip(ex_names, axs.flat):
         do_it_all(ax, base_dir_real, base_dir_sim, i)
