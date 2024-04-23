8e552849e225be671d64d1d16966ae5b2cdf5920
Henrik Mellum <henmell99@gmail.com>
2024-04-23 15:44:39 +0200

    Etter test timescale=1

--- Python/test_individ2.py
+++ Python/test_individ2.py
@@ -17,8 +17,36 @@ import pandas as pd
 from ast import literal_eval
 
 
-directory = "../../Master_Resultater/Determ/G_T_B_exLimit/1"
-directory = "result/time_scale1"
+#directory = "../../Master_Resultater/Determ/G_T_B_exLimit/1"
+#directory = "result/time_scale1"
+
+
+directory = "../../timescale/timescale/kontrollers_timescale/G_S_B_exLimit/102"
+directory = "../../timescale/timescale/kontrollers_timescale/G_SUfq_B_exLimit/341"
+directory = "../../timescale/timescale/kontrollers_timescale/G_T_B_exLimit/121"
+directory = "../../timescale/timescale/kontrollers_timescale/G_TWoff_B_exLimit/363"
+directory = "../../timescale/timescale/kontrollers_timescale/G_TWoffFq_B_exLimit/326"
+
+#directory = "../../timescale/timescale/kontrollers_timescale/T_S_B_exLimit/"
+directory = "../../timescale/timescale/kontrollers_timescale/T_SUfq_B_exLimit/280"
+directory = "../../timescale/timescale/kontrollers_timescale/T_T_B_exLimit/281"
+directory = "../../timescale/timescale/kontrollers_timescale/T_TWoff_B_exLimit/301"
+#directory = "../../timescale/timescale/kontrollers_timescale/T_TWoffFq_B_exLimit/"
+
+directory = "../../timescale/timescale/kontrollers_timescale/Z_S_B_exLimit/4"
+directory = "../../timescale/timescale/kontrollers_timescale/Z_SUfq_B_exLimit/21"
+directory = "../../timescale/timescale/kontrollers_timescale/Z_T_B_exLimit/42"
+directory = "../../timescale/timescale/kontrollers_timescale/Z_TWoff_B_exLimit/61"
+directory = "../../timescale/timescale/kontrollers_timescale/Z_TWoffFq_B_exLimit/81"
+
+directory = "../../timescale/timescale/miljo_timescale/Z_TWoff_B_CS0/1"
+directory = "../../timescale/timescale/miljo_timescale/Z_TWoff_B_CS1/416"
+#directory = "../../timescale/timescale/miljo_timescale/Z_TWoff_B_CS2/"
+directory = "../../timescale/timescale/miljo_timescale/Z_TWoff_B_CS5/458"
+#directory = "../../timescale/timescale/miljo_timescale/Z_TWoff_B_HCS1/1"
+directory = "../../timescale/timescale/miljo_timescale/Z_TWoff_B_HCS2/381"
+directory = "../../timescale/timescale/miljo_timescale/Z_TWoff_B_HCS5/221"
+
 d, config = get_one_dataframes(directory, "grid.solutions.csv", parse=True)
 
 arr2 = d.to_numpy()
@@ -32,7 +60,7 @@ fitnessfunction = get_fitnessfunction(config=config)
 
 print("\n"*10)
 test = (18, 10)
-test = (16, 11) #Ikke veldig bra med G_S_B
+test = (10, 16) #Ikke veldig bra med G_S_B
 x = test[0]
 y = test[1]
 ind = arr2[x][y]
@@ -43,12 +71,11 @@ print("Directory:", directory)
 print("X:", x, "Y:", y)
 print("Genom", genom)
 print("\n")
-
 try:
     # Create the channel
     if "Qutee" in config:
         qutee_config = config["Qutee"]
-        #qutee_config["CubeCount"] = 0
+        qutee_config["CubeCount"] = 0
     else:
         qutee_config = None 
     print(qutee_config)
@@ -60,9 +87,9 @@ try:
     b=[]
     c=[]
 
-    for i in range(3):
+    for i in range(1):
         csvPath = str(i) + "_gang.csv"
-        svar = env.evaluate(genom, csvPath=csvPath)
+        svar = env.evaluate(genom, csvPath=None, realRobot=True)
         print("Skulle:", (ind[0]["fitness"], ind[0]["features"]))
         print("Endte :",svar)
         diff = np.interp(ind[0]["fitness"]-svar[0][0], [-1, 1], [-180, 180])
